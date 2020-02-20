81d5ba30b5f37b324f307fea5c5ce0e2c493a91b
Marian Fisk <mamafisk33@gmail.com>
2020-02-19 21:23:20 -0800

    NPM Scripts Part 1 - Week 4-10

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(function() {
+  $(".carousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function() {
+    if (
+      $("#carouselButton")
+        .children("i")
+        .hasClass("fa-pause")
+    ) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton")
+        .children("i")
+        .removeClass("fa-pause");
+      $("#carouselButton")
+        .children("i")
+        .addClass("fa-play");
+    } else {
+      $(".carousel").carousel("next"); //when click on play button, transition immediately to next pic instead of waiting first
+      $(".carousel").carousel("cycle");
+      $("#carouselButton")
+        .children("i")
+        .removeClass("fa-play");
+      $("#carouselButton")
+        .children("i")
+        .addClass("fa-pause");
+    }
+  });
+});
